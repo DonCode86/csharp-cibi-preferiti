e15db3af124004119d0331ececbdfeaec46b54c9
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-15 15:48:25 +0200

    fixed-list-dishes

--- Program.cs
+++ Program.cs
@@ -5,12 +5,14 @@ using System.Reflection;
 string[] array = new string[4];
 string[] favoriteDishes = new string[] { "Pasta", "Pizza", "Carne", "Gelato", "Pesce" };
 //stampo lunghezza Array
-Console.Write("Il numero totale dei tuoi piatti preferiti è:" + favoriteDishes.Length);
+Console.Write("Il numero totale dei tuoi piatti preferiti è: " + favoriteDishes.Length);
 Console.WriteLine("");
 //stampa lista piatti
 for (int i = 0; i < favoriteDishes.Length; i++)
 {
-    Console.WriteLine(String.Format(" favoriteDishes[{0}] = {1}", i+1, favoriteDishes[i]));
+    Console.WriteLine(String.Format("piatto preferito[{0}] = {1}", i+1, favoriteDishes[i]));
 }
 Console.WriteLine("il tuo cibo preferito è: " + favoriteDishes[0]);
 Console.WriteLine("il tuo cibo meno preferito è: " + favoriteDishes[4]);
+
+
